1ff786e066b5fcf74f16c329bb8c6436289f19df
Avery Larsen <averylarsen@mailbox.org>
2023-12-03 12:12:35 -0800

    Refactor

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -2,57 +2,61 @@ use std::collections::HashSet;
 
 advent_of_code::solution!(3);
 
-trait IsSymbol {
-    fn is_symbol(&self) -> bool;
+type Schematic = Vec<Vec<char>>;
+
+fn parse(input: &str) -> Schematic {
+    input.lines().map(|line| line.chars().collect()).collect()
 }
 
-impl IsSymbol for char {
-    fn is_symbol(&self) -> bool {
-        !(self.is_ascii_digit() || *self == '.')
+fn is_symbol(c: char) -> bool {
+    !(c.is_ascii_digit() || c == '.')
+}
+
+fn find_part_number(sch: &Schematic, row: usize, col: usize, cols: usize) -> u32 {
+    let mut start_col = col;
+    while start_col > 0 && sch[row][start_col - 1].is_ascii_digit() {
+        start_col -= 1;
     }
+    let mut end_col = col;
+    while end_col < cols && sch[row][end_col].is_ascii_digit() {
+        end_col += 1;
+    }
+    let mut part_number = String::new();
+    for i_col in start_col..end_col {
+        part_number.push(sch[row][i_col]);
+    }
+    part_number.parse().unwrap()
+}
+
+fn find_part_numbers(sch: &Schematic, row: usize, col: usize, cols: usize) -> Vec<u32> {
+    #[rustfmt::skip]
+    static NEIGHBORS: [(i32, i32); 8] = [
+        (-1, -1), (-1, 0), (-1, 1),
+        ( 0, -1),          ( 0, 1),
+        ( 1, -1), ( 1, 0), ( 1, 1),
+    ];
+    let mut part_numbers = HashSet::new();
+    for (n_row, n_col) in NEIGHBORS
+        .map(|(d_row, d_col)| ((row as i32 + d_row) as usize, (col as i32 + d_col) as usize))
+    {
+        if sch[n_row][n_col].is_ascii_digit() {
+            let part_number = find_part_number(sch, n_row, n_col, cols);
+            part_numbers.insert(part_number);
+        }
+    }
+    part_numbers.into_iter().collect()
 }
 
 pub fn part_one(input: &str) -> Option<String> {
-    let sch: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    let sch = parse(input);
     let rows = sch.len();
     let cols = sch[0].len();
     let mut sum = 0;
 
-    let neighbors = [
-        (-1, -1),
-        (-1, 0),
-        (-1, 1),
-        (0, -1),
-        (0, 1),
-        (1, -1),
-        (1, 0),
-        (1, 1),
-    ];
-
     for row in 0..rows {
         for col in 0..cols {
-            if sch[row][col].is_symbol() {
-                let mut part_numbers = HashSet::new();
-                for (n_row, n_col) in neighbors.map(|(d_row, d_col)| {
-                    ((row as i32 + d_row) as usize, (col as i32 + d_col) as usize)
-                }) {
-                    if sch[n_row][n_col].is_ascii_digit() {
-                        let mut start_col = n_col;
-                        while start_col > 0 && sch[n_row][start_col - 1].is_ascii_digit() {
-                            start_col -= 1;
-                        }
-                        let mut end_col = n_col;
-                        while end_col < cols && sch[n_row][end_col].is_ascii_digit() {
-                            end_col += 1;
-                        }
-                        let mut part_number = String::new();
-                        for i_col in start_col..end_col {
-                            part_number.push(sch[n_row][i_col]);
-                        }
-                        let part_number: u32 = part_number.parse().unwrap();
-                        part_numbers.insert(part_number);
-                    }
-                }
+            if is_symbol(sch[row][col]) {
+                let part_numbers = find_part_numbers(&sch, row, col, cols);
                 sum += part_numbers.iter().sum::<u32>();
             }
         }
@@ -62,46 +66,15 @@ pub fn part_one(input: &str) -> Option<String> {
 }
 
 pub fn part_two(input: &str) -> Option<String> {
-    let sch: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    let sch = parse(input);
     let rows = sch.len();
     let cols = sch[0].len();
     let mut sum = 0;
 
-    let neighbors = [
-        (-1, -1),
-        (-1, 0),
-        (-1, 1),
-        (0, -1),
-        (0, 1),
-        (1, -1),
-        (1, 0),
-        (1, 1),
-    ];
-
     for row in 0..rows {
         for col in 0..cols {
             if sch[row][col] == '*' {
-                let mut part_numbers = HashSet::new();
-                for (n_row, n_col) in neighbors.map(|(d_row, d_col)| {
-                    ((row as i32 + d_row) as usize, (col as i32 + d_col) as usize)
-                }) {
-                    if sch[n_row][n_col].is_ascii_digit() {
-                        let mut start_col = n_col;
-                        while start_col > 0 && sch[n_row][start_col - 1].is_ascii_digit() {
-                            start_col -= 1;
-                        }
-                        let mut end_col = n_col;
-                        while end_col < cols && sch[n_row][end_col].is_ascii_digit() {
-                            end_col += 1;
-                        }
-                        let mut part_number = String::new();
-                        for i_col in start_col..end_col {
-                            part_number.push(sch[n_row][i_col]);
-                        }
-                        let part_number: u32 = part_number.parse().unwrap();
-                        part_numbers.insert(part_number);
-                    }
-                }
+                let part_numbers = find_part_numbers(&sch, row, col, cols);
                 if part_numbers.len() == 2 {
                     sum += part_numbers.iter().product::<u32>();
                 }
